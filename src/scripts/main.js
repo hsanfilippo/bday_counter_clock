3ccee1f2e6d8a79c4ab9a1fbacebd132098b2b65
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2023-10-15 20:21:10 +0900

    countdown set on main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,23 @@
 AOS.init();
 
-const bDay = new Date("Dec 27, 2023, 21:00:00");
-const bDayStamp = bDay.getTime();
+const bday = new Date("Dec 27, 2023, 21:00:00");
+const bdayStamp = bday.getTime();
+
+const countdown = setInterval(function() {
+    const actualTime = new Date();
+    const actualTimeStamp = actualTime.getTime();
+
+    const bdayTimeLeft = bdayStamp - actualTimeStamp;
+
+    const msDay = 1000 * 60 * 60 * 24;
+    const msHour = 1000 * 60 * 60;
+    const msMin = 1000 * 60;
+
+    const daysTillbday = Math.floor(bdayTimeLeft / msDay);
+    const hrsTillbday = Math.floor((bdayTimeLeft % msDay) / msHour);
+    const mntTillbday = Math.floor((bdayTimeLeft % msHour) / msMin);
+    const secTillbday = Math.floor((bdayTimeLeft % msMin) / 1000);
+
+    console.log(secTillbday);
+    document.getElementById('clock').innerHTML = `${daysTillbday} dias e ${hrsTillbday}:${mntTillbday}:${secTillbday}`
+}, 1000);
